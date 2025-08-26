df68ae2bd7954239628fd9242604a203dd918872
SDNATH-Git <sd.nath1952@gmail.com>
2025-08-26 13:33:55 +0600

    added a resume

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,27 +1,3 @@
-// import React from "react";
-// import { motion } from "framer-motion";
-// import loaderPng from "../assets/logo.png";
-
-// const Loader = () => {
-//     return (
-//         <div className="fixed inset-0 bg-gray-900 bg-opacity-90 flex flex-col justify-center items-center z-50 ">
-
-//             <motion.img
-//                 src={loaderPng}
-//                 alt="Loading..."
-//                 className="w-30 h-30 mb-4 p-4 rounded-full border-4 border-orange-500"
-//                 animate={{ rotate: 360 }}
-//                 transition={{ repeat: Infinity, duration: 2, ease: "linear" }}
-//             />
-
-//             <p className="text-orange-500 text-xl font-semibold">Loading...</p>
-//         </div>
-//     );
-// };
-
-// export default Loader;
-
-
 import React from "react";
 import { motion } from "framer-motion";
 import Logo from "../assets/logo.png";
